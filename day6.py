dac091100d94738dd4c7843aebed11facab76809
rahul-mendes <31122814+rahul-mendes@users.noreply.github.com>
2023-01-12 01:02:24 -0500

    Day 6: Revorg Maze

---
+++ day6.py
@@ -0,0 +1,45 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    move()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while front_is_clear():
+    move()
+turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+"""

5d7835fee88831c5cdcf24c37f9750310bd0cf50
Rahul Mendes <rahul.mendes@gmail.com>
2024-04-22 15:05:47 -0400

    Create speed.py

---
+++ day54/speed.py
@@ -0,0 +1,22 @@
+import time
+current_time = time.time()
+print(current_time) # seconds since Jan 1st, 1970 
+
+# Write your code below 👇
+
+def speed_calc_decorator(function):
+   def wrapper_function():
+     start_time=time.time()
+     function()
+     end_time=time.time()
+     print(f"{function.__name__} run speed: {end_time - start_time}s")
+   return wrapper_function      
+
+def fast_function():
+  for i in range(1000000):
+    i * i
+        
+
+def slow_function():
+  for i in range(10000000):
+    i * i
